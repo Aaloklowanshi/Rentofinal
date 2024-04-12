a80d60cced5712ae4169d460c1772240e6338a95
Aaloklowanshi <lowanshiaalok@gmail.com>
2024-04-12 14:07:31 +0530

    done

--- server/index.js
+++ server/index.js
@@ -1,26 +1,23 @@
 const mongoose = require('mongoose');
-const express  = require('express');
-const { User } = require("./db");
+const express = require('express');
 const app = express();
-const authRoute = require("./routes/auth");
-
-
-const port = 8000;
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
-const cors = require("cors"); 
+const cors = require("cors");
 
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://om:Ye6hkj4c3OvOtNYl@cluster0.ynonr4i.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true, dbName: "Rento-Db" });
+
+// Middleware
 app.use(cors());
-
 app.use(express.json());
+
+// Routes
 app.use("/auth", authRoutes);
 app.use("/user", userRoutes);
 
-
+// Start the server
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Server is running on port ${port}`);
 });
-
-mongoose.connect('mongodb+srv://om:Ye6hkj4c3OvOtNYl@cluster0.ynonr4i.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true, dbName: "Rento-Db" });
-
-
